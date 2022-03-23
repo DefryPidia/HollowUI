b24cc0f2fe9b783db180d18d30e3e1dd23da1f4d
dickylkmnh <dickylukmanhakim03@gmail.com>
2022-03-23 11:01:22 +0700

    init

--- src/layouts/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class LayoutsIndexComponent extends React.Component {
-    render() {
-        return (
-            <div>okok</div>
-        )
-    }
-}
