1e7176bc852e18b7519d82096a5df23d2ff6ece0
DefryPidia <realdefrifidia@gmail.com>
2022-03-24 14:01:41 +0700

    Defry | deploy to vercel

--- src/layouts/footer/layouts-footer.component.tsx
+++ src/layouts/footer/layouts-footer.component.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export const LayoutsFooterComponent: React.FC = () => {
     return (
         <footer className="justify-center text-center text-red-700 bg-gray-400">
